1e308657afd05b3feae19c2f0051622e61c423d6
amadiot <amadiot@roi.educ.insa>
2014-01-21 09:43:33 +0100

    [PIT] Changement du projet en projet Maven + premiers tests

--- PIT/src/vvt/pit/code/FirstClass.java
+++ PIT/src/vvt/pit/code/FirstClass.java
@@ -3,14 +3,27 @@ package vvt.pit.code;
 public class FirstClass {
 	
 	private boolean myBool;
+	private int cmpt;
 
 	public boolean isMyBool() {
 		return myBool;
 	}
-
-	public FirstClass(boolean myBool) {
-		this.myBool = myBool;
+	
+	public void setMyBool(boolean b){
+		myBool = b;
 	}
+	
+	public int getCmpt(){
+		return cmpt;
+	}
+	
+	public void setCmpt(int a ){
+		cmpt = a;
+		cmpt ++;
+	}
+
+	
+	
 	
 	
 
--- PIT/src/vvt/pit/test/FirstTest.java
+++ PIT/src/vvt/pit/test/FirstTest.java
@@ -2,16 +2,38 @@ package vvt.pit.test;
 
 import static org.junit.Assert.*;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import vvt.pit.code.FirstClass;
 
 public class FirstTest {
+
+	FirstClass c;
+	
+	@Before
+	public void init() {
+		c = new FirstClass();
+	}
+	
+	@After
+	public void TearDown(){
+		c = null;
+	}
+
 	@Test
 	public void testFirtClass() {
-		boolean test= new FirstClass(false).isMyBool();
+		c.setMyBool(false);
+		boolean test = c.isMyBool();
 		assertFalse(test);
-		
+
+	}
+	
+	@Test
+	public void testCmpt(){
+		c.setCmpt(10);
+		assertEquals(11, c.getCmpt());
 	}
 
 }
